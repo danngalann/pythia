c41ad1b31f0105e35f522b406e8374a32f6e5493
Daniel Galán <47415745+danngalann@users.noreply.github.com>
2023-05-16 17:03:17 +0200

    Use FAISS to answer query embeddings from RAM

--- api/app/main.py
+++ api/app/main.py
@@ -2,4 +2,6 @@ from services.ingest.document.PDFIngestor import PDFIngestor
 
 if __name__ == "__main__":
     pdf_ingestor = PDFIngestor()
-    pdf_ingestor.ingest("/home/daniel/Documents/Attention Is All You Need.pdf")
+    document = pdf_ingestor.ingest("/home/daniel/Documents/Attention Is All You Need.pdf")
+    docs = pdf_ingestor.search(document, "What are transformers?")
+    print(docs)
--- api/app/services/ingest/document/PDFIngestor.py
+++ api/app/services/ingest/document/PDFIngestor.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from .DocumentIngestor import DocumentIngestor
 from PyPDF2 import PdfReader
 from langchain.embeddings import HuggingFaceEmbeddings
@@ -12,7 +14,13 @@ class PDFIngestor(DocumentIngestor):
         """Ingests a PDF document into the database"""
         reader = PdfReader(document_path)
         raw_text = self.__get_raw_text(reader)
-        print(raw_text)
+        split_texts = self.__split_text(raw_text)
+        document = self.__get_embeddings(split_texts)
+
+        return document
+
+    def search(self, document: FAISS, query: str) -> List[str]:
+        return [doc.page_content for doc in document.similarity_search(query)]
 
     def __get_raw_text(self, reader: PdfReader) -> str:
         """Gets the raw text from a PDF document"""
@@ -21,3 +29,18 @@ class PDFIngestor(DocumentIngestor):
             raw_text += page.extract_text()
 
         return raw_text
+
+    def __split_text(self, raw_text: str) -> List[str]:
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+        )
+
+        return text_splitter.split_text(raw_text)
+
+    def __get_embeddings(self, texts: List[str]) -> FAISS:
+        """Gets the embeddings for a text"""
+        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+        return FAISS.from_texts(texts, embeddings)
